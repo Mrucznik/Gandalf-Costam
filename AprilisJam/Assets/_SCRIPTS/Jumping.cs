ac72b185e6f9c880f9e19917bec2c0212eafc5e3
Unknown <202254@edu.p.lodz.pl>
2017-04-01 19:10:30 +0200

    Gandalf is ready

--- AprilisJam/Assets/_SCRIPTS/Jumping.cs
+++ AprilisJam/Assets/_SCRIPTS/Jumping.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +8,13 @@ public class Jumping : MonoBehaviour
 
     int jump2 = 0, floor = 0;
     public Rigidbody2D rb;
+    private Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-       
+        anim = GetComponent<Animator>();
+
     }
 
     void Update()
@@ -19,6 +22,9 @@ public class Jumping : MonoBehaviour
 
         if (Input.GetKeyDown("space") && ((floor == 1 || jump2 <= 1)))
         {
+
+            anim.SetBool("jump",true);
+        
             rb.AddForce(new Vector2(0, 200f));
             floor = 0;
             jump2++;
@@ -29,6 +35,7 @@ public class Jumping : MonoBehaviour
     {
         floor = 1;
         jump2 = 0;
+        anim.SetBool("jump", false);
 
     }
 }
--- AprilisJam/Assets/_SCRIPTS/PlayerMove.cs
+++ AprilisJam/Assets/_SCRIPTS/PlayerMove.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Security;
 using System.Timers;
 
 public class PlayerMove : MonoBehaviour
 {
     public int obrot = 0, znak = 1;
-  
+    private Animator anim;
+    private int moveiterator = 0;
+
     // Use this for initialization
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -17,11 +20,27 @@ public class PlayerMove : MonoBehaviour
 
     }
 
+    
     public float speed = 1.0F;
     void Update()
     {
-        float translationH = Input.GetAxis("Horizontal") * 2;
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A))
+        {
+
+            moveiterator++;
+           
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A)) moveiterator--; ;
+        anim.SetInteger("walk", moveiterator);
+
+
+        float translationH = Input.GetAxis("Horizontal") * 2;  // pobiera sterowanie     
         translationH *= Time.deltaTime;
+
+
         if (translationH < 0 && obrot < 1)
         {
             obrot = 1;
